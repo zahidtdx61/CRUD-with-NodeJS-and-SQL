4351264dc94e0f5515964b5d3fc1ec337215e7e4
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-18 19:23:22 +0600

    added initials

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const { ServerConfig, Logger } = require("./config");
+const apiRoutes = require("./routes");
+
+const app = express();
+
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully running the server on port: ${ServerConfig.PORT}`);
+  Logger.info(`Successfully running the server on port: ${ServerConfig.PORT}`);
+});
