96158732854cb8d6345bb76645e80f8752c60459
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-19 20:33:37 +0600

    Refactor the indentation in the `index.js` file for better migration file for creating the `Airplanes` table - Define the `Airplane` model with the correct data types and associations

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,4 +4,7 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
 };
---
+++ src/migrations/20240518133948-create-airplane.js
@@ -0,0 +1,34 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Airplanes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      modelNUmber: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      capacity: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Airplanes");
+  },
+};
