eaee6717b01ce6334de33df1c020400d51811490
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-19 22:19:22 +0600

    added a create controller for airplane

---
+++ src/controllers/airplane-controller.js
@@ -0,0 +1,38 @@
+const { StatusCodes } = require("http-status-codes");
+const { AirplaneService } = require("../services");
+const { error } = require("winston");
+
+/***
+ * POST: /airplanes
+ * req-body {modelNumber: 'air-bd420', capacity: 200}
+ */
+async function createAirplane(req, res) {
+  try {
+    const airplane = await AirplaneService.createAirplane({
+      modelNumber: req.body.modelNumber,
+      capacity: req.body.capacity,
+    });
+
+    return res
+            .status(StatusCodes.CREATED)
+            .json({
+              success: true,
+              message: "Successfully created an airplane",
+              data: airplane,
+              error: {},
+            });
+  } catch (error) {
+    return res
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json({
+              success: false,
+              message: 'Something went wrong while creating an airplane',
+              data: {},
+              error: error,
+            })
+  }
+}
+
+module.exports = {
+  createAirplane,
+};
