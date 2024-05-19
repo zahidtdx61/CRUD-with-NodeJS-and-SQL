5d8c3dd4937f6ae8832d01ccfbe1a040c9da60b6
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-19 22:29:40 +0600

    added routes for airplane

---
+++ src/routes/v1/airplane-routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router;
+
+const { AirplaneController } = require("../../controllers");
+
+router.post("/", AirplaneController.createAirplane);
+
+module.exports = router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { InfoController } = require("../../controllers");
 const router = express.Router();
 
+const { InfoController } = require("../../controllers");
+const airplaneRoutes = require("./airplane-routes");
+
 router.get("/info", InfoController.info);
+router.use("/airplane", airplaneRoutes);
 
 module.exports = router;
